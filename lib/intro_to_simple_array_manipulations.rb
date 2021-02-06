c2dcdc516368d950ae9c874c6d73a9546b673a9c
Seth Cohen <smcohen82@gmail.com>
2021-02-06 04:30:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(arr, el)
+  arr.push(el)
+end
+
+def using_unshift(arr, el)
+  arr.unshift(el)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
